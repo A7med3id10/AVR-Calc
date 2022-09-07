cd1ae9a784e0a2193ca546480d9daae41bb14630
Ahmed Eid <66841732+A7med3id10@users.noreply.github.com>
2022-09-07 02:32:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,15 +6,7 @@
  */
 
 #include"APP/Calculator/Calculator_header.h"
-#include"APP/APP_LCD/APP_LCD.h"
-/*#include"LIB/STD_TYPES.h"
-#include"LIB/BIT_MATH.h"
-#define F_CPU 8000000UL
-#include <util/delay.h>
-#include"EHAL/LCD/header/LCD_interface.h"
-#include"MCAL/DIO/header/DIO_interface.h"*/
 
 int main() {
 	Calculator();
-	//project_LCD();
 }
